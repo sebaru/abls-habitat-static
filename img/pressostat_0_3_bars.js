701b5725abf8c46bf82564577a1fc5fbffbd056b
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2024-01-02 11:50:25 +0100

    ICON: Add pressostat_0_3_bars

---
+++ img/pressostat_0_3_bars.js
@@ -0,0 +1,7 @@
+     console.log ( "Inside Set_State of Pressostat_simple. Args = " + state );
+     aiguille = this.svggroupe.find("#path5908")[0];
+/*     console.log ( aiguille );
+     console.log ( aiguille.node );*/
+     var degree = 270*state.valeur/3;
+     aiguille./*animate(2000).*/transform ( { rotate: degree, origin: { x: 64.824, y: 64.774 } } );
+     if ( state.color != null ) aiguille.css("fill", state.color );
